f609c8c40e30e0868d848a3085f1219e48d8a93f
andres vasquez <andreusharp@gmail.com>
2023-09-05 10:43:25 +0200

    Cambios

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
-import { PortfolioRoutes } from '../portfolio/routes/portfolioRoutes'
+import { PortfolioRoutes } from "../portfolio/routes/portfolioRoutes"
 
 export const AppRouter = () => {
   return (
     <Routes>
         
-        <Route path="/*" element={ <PortfolioRoutes/> } />
+        <Route path="/" element={ <PortfolioRoutes/> } />
 
     </Routes>
   )
