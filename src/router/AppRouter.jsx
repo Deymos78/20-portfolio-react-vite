05d33a48a7415f7769b6d651ed3c2a0f14407a8a
andres vasquez <andreusharp@gmail.com>
2023-09-05 11:28:08 +0200

    Nuevas mejoras secundarias

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
-import { PortfolioRoutes  } from "../portfolio/routes/PortfolioRoutes"
-
+import {PortfolioRoutes} from '../portfolio/routes/PortfolioRoutes'
 export const AppRouter = () => {
   return (
     <Routes>
         
-        <Route path="/" element={ <PortfolioRoutes/> } />
+        <Route path="/*" element={ <PortfolioRoutes/>  } />
 
     </Routes>
   )
